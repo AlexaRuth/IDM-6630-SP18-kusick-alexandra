8c4284e3488aa667dc444d971f26d77bbf17bda5
Alex Kusick <Akusick@mcad.edu>
2018-01-23 21:16:11 -0600

    Slack Test 001

---
+++ KUSICK_alex_assignment_001/code/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>WK1 - Assignment Page</title>
+  </head>
+  <body>
+     <?php echo "<h1> Cora Rose Fox </h1>"; ?>
+     <nav><?php include "inc/nav.php" ?></nav>
+      
+      <img src="../img/cora.jpg" alt="cora the dog">
+      
+  </body>
+</html>
