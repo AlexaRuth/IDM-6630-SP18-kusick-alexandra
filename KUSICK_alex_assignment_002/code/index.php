16fe21f60720ff8859d2d92e2d661aa10f0f4815
Alex Kusick <Akusick@mcad.edu>
2018-02-01 19:30:38 -0600

    Updates

--- KUSICK_alex_assignment_002/code/index.php
+++ KUSICK_alex_assignment_002/code/index.php
@@ -1,42 +1,44 @@
-//<?php
-
-// SCRIPT
-
-//echo "<ul>";
-	//$query_array = $_GET;
-
-	//print_r(query_array); test for names values or both...
-
-		//foreach ($query_array as $key_name => $key_value) {
-			//echo "<li>" . $key_name . ": ";
-			//echo $key_value . "</li>";
-		//}
-
-//echo "</ul>" 
-//?>
-
-
 <?php
 
 // SCRIPT
 
+$queryarray = $_GET;
+
+	//print_r(query_array); test for names values or both...
+
+foreach ($queryarray as $keyname => $bool) {
+    
+    if ($bool == true){
+        
+     echo "variable value," . $keyname .", " "value is true";
+ } else{
+     echo echo "variables value," . $keyname .", " " value is false";
+ } 
+}
+
+?>
+
+
+<?php
+
+// SCRIPT NOTES
+
  
-// NEGSATIVE - 
+// NEGATIVE - 
 // $test =50;   
 // if ($test != 100) {
 // echo "The number is not 100!";
 // }   
  
-
-$test =100;
-$string = "this is just a test!";
-$bool = true;
+// $test =100;
+// $string = "this is just a test!";
+// $bool = true;
     
- if ($bool == true) {
-     echo "The value was true!";
- } else{
-     echo "The value was false!";
- } 
+// if ($bool == true) {
+// echo "The value was true!";
+// } else{
+// echo "The value was false!";
+// } 
         
 
 ?>
