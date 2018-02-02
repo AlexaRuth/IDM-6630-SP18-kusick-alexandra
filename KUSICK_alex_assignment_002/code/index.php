197514102fe3b391467eb634fb165c42883beed9
Alex Kusick <Akusick@mcad.edu>
2018-02-01 19:57:32 -0600

    FINAL COMMIT CHECKIN 3.2

--- KUSICK_alex_assignment_002/code/index.php
+++ KUSICK_alex_assignment_002/code/index.php
@@ -2,44 +2,22 @@
 
 // SCRIPT
 
-$queryarray = $_GET;
+$queryArray = $_GET;
 
 	//print_r(query_array); test for names values or both...
-
-foreach ($queryarray as $keyname => $bool) {
+    
+foreach ($queryArray as $keyName => $bool) {
+    
+$bool = true;
     
     if ($bool == true){
         
-     echo "variable value," . $keyname .", " "value is true";
- } else{
-     echo echo "variables value," . $keyname .", " " value is false";
+     echo "variable", ".' $keyName '.", "The value is true.";
+     } else{
+     echo "variable", ".' $keyName '.", "The value is false.";
  } 
 }
 
 ?>
 
 
-<?php
-
-// SCRIPT NOTES
-
- 
-// NEGATIVE - 
-// $test =50;   
-// if ($test != 100) {
-// echo "The number is not 100!";
-// }   
- 
-// $test =100;
-// $string = "this is just a test!";
-// $bool = true;
-    
-// if ($bool == true) {
-// echo "The value was true!";
-// } else{
-// echo "The value was false!";
-// } 
-        
-
-?>
-          
