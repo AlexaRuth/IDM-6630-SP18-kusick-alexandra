537566500eecd17ad60b08777cce30770cff23e7
Alex Kusick <Akusick@mcad.edu>
2018-02-01 19:16:12 -0600

    INITIAL COMMIT

--- KUSICK_alex_assignment_002/code/index.php
+++ KUSICK_alex_assignment_002/code/index.php
@@ -1,20 +1,43 @@
+//<?php
+
+// SCRIPT
+
+//echo "<ul>";
+	//$query_array = $_GET;
+
+	//print_r(query_array); test for names values or both...
+
+		//foreach ($query_array as $key_name => $key_value) {
+			//echo "<li>" . $key_name . ": ";
+			//echo $key_value . "</li>";
+		//}
+
+//echo "</ul>" 
+//?>
+
+
 <?php
 
 // SCRIPT
 
-echo "<ul>";
-
-	$query_array = $_GET;
-
-	//print_r(query_array); test for names values or both...
-
-		foreach ($query_array as $key_name => $key_value) {
-			echo "<li>" . $key_name . ": ";
-			echo $key_value . "</li>";
-		}
-
-echo "</ul>"
-	
  
+// NEGSATIVE - 
+// $test =50;   
+// if ($test != 100) {
+// echo "The number is not 100!";
+// }   
+ 
+
+$test =100;
+$string = "this is just a test!";
+$bool = true;
+    
+ if ($bool == true) {
+     echo "The value was true!";
+ } else{
+     echo "The value was false!";
+ } 
+        
+
 ?>
           
